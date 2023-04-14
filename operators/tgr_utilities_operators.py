2344567a2a4fd8211fd46b2f97df1356f5b43f1f
Matheus Santos <matheus.santos@telergystudio.com>
2023-04-14 16:32:48 -0300

    UTILITIES: Fixed Select by Layer in pose mode

--- operators/tgr_utilities_operators.py
+++ operators/tgr_utilities_operators.py
@@ -318,55 +318,6 @@ class TGR_OT_CleanNameUp(bpy.types.Operator):
 
 # ------------- SELECTION -------------
 class TGR_OT_SelectBonesByName(bpy.types.Operator):
-    """Select all the bones with the matching name"""
-    bl_idname = "tgr.select_bones_by_name"
-    bl_label = "Select Bones by Name"
-    bl_options = {"REGISTER", "UNDO"}
-    
-    bone_name: bpy.props.StringProperty(name="Search Name")
-    
-    def __init__(self):
-        self.shift = False
-    
-    @classmethod
-    def poll(cls, context):
-        # Check if the selected object is an Armature
-        is_armature = context.object.type == 'ARMATURE'
-        # Check if the selected object is in Edit Mode or Pose Mode
-        is_edit_mode = context.mode == 'EDIT_ARMATURE'
-        is_pose_mode = context.mode == 'POSE'
-        # Check if the selected object is an Armature and in Edit Mode or Pose Mode
-        return is_armature and (is_edit_mode or is_pose_mode)
-    
-    def execute(self, context):
-        if not self.shift:
-            # Deselect all bones
-            if context.mode == 'EDIT_ARMATURE':
-                bpy.ops.armature.select_all(action='DESELECT')
-            elif context.mode == 'POSE':
-                bpy.ops.pose.select_all(action='DESELECT')
-        
-        # Select all the bones with the matching name
-        if context.mode == 'EDIT_ARMATURE':
-            for bone in context.object.data.edit_bones:
-                if self.bone_name in bone.name:
-                    bone.select = True
-                    bone.select_head = True
-                    bone.select_tail = True
-        elif context.mode == 'POSE':
-            for bone in context.object.pose.bones:
-                if self.bone_name in bone.name:
-                    bone.bone.select = True
-            
-        return {"FINISHED"}
-    
-    def invoke(self, context, event):
-        # Check if the shift key is pressed
-        self.shift = event.shift
-        return self.execute(context)
-    
-    
-class TGR_OT_SelectBonesByName(bpy.types.Operator):
     """
     Select all the bones of that match the searched name.
     """
@@ -448,11 +399,16 @@ class TGR_OT_SelectLayerBones(bpy.types.Operator):
                 bpy.ops.pose.select_all(action='DESELECT')
         
         # Select all the bones of the specified layer
-        for bone in context.object.data.edit_bones:
-            if bone.layers[self.layer]:
-                bone.select = True
-                bone.select_head = True
-                bone.select_tail = True
+        if context.mode == 'EDIT_ARMATURE':
+            for edit_bone in context.active_object.data.edit_bones:
+                if edit_bone.layers[self.layer]:
+                    edit_bone.select = True
+                    edit_bone.select_head = True
+                    edit_bone.select_tail = True
+        elif context.mode == 'POSE':
+            for pose_bone in context.active_object.pose.bones:
+                if pose_bone.bone.layers[self.layer]:
+                    pose_bone.bone.select = True
             
         return {"FINISHED"}
     
