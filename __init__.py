6a5cec0eb203987fb9cc335c8c9989ee64ed9a37
Matheus Santos <matheus.santos@telergystudio.com>
2024-05-24 14:10:48 -0300

    Update Blender compatibility to version 4.1

--- __init__.py
+++ __init__.py
@@ -100,7 +100,7 @@ bl_info = {
     "author": "Telergy Studio",
     "description": "Supercharge your rigging workflow! Accelerate the process, automate tasks, and unleash your"
                    " creative potential like never before.",
-    "blender": (4, 0, 0),
+    "blender": (4, 1, 0),
     "version": (0, 1, 0),
     "location": "",
     "warning": "Under Heavy Development",
