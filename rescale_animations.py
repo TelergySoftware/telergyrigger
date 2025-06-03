b54f22887bcb4fb2214cf5d6809c067a57acab5d
Matheus Santos <matheus.santos@telergystudio.com>
2025-06-03 08:19:17 -0300

    feat: Update armature reference and scale factor in rescale_animations script

--- rescale_animations.py
+++ rescale_animations.py
@@ -1,17 +1,29 @@
 import bpy
 from bpy import data as D
 
-armature = D.objects['Heli_01']
-scale_factor = 0.04
+armature = D.objects['Armature']
+scale_factor = .01 # Scale factor to apply to the armature and animations
+# Get the actions associated with the armature
 actions = D.actions
 
 
+# Scale the armature object by the scale factor
+armature.scale = (scale_factor, scale_factor, scale_factor)
+# Deselect all objects
+bpy.ops.object.select_all(action='DESELECT')
+# Select the armature object
+armature.select_set(True)
+# Apply the scale to the object
+bpy.ops.object.transform_apply(scale=True)
+
+for bone in armature.pose.bones:
+    # If the bone has a stretch to constraint, multiply the rest length by the scale factor
+    for constraint in bone.constraints:
+        if constraint.type == 'STRETCH_TO':
+            constraint.rest_length *= scale_factor
+        
 for action in actions:
     # Scale the location keyframes and the handles by the scale factor
-    # Continue if the action name is H_Idle
-    print(action.name)
-    if action.name == 'H_Idle':
-        continue
     print('Rescaling action: ', action.name)
     for fcurve in action.fcurves:
         if 'location' in fcurve.data_path:
@@ -19,4 +31,3 @@ for action in actions:
                 keyframe.co[1] *= scale_factor
                 keyframe.handle_left[1] *= scale_factor
                 keyframe.handle_right[1] *= scale_factor
-                
