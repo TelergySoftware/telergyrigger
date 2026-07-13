28cf612c65d654734b66cee787921b9f5e87e2ab
Matheus Santos <matheus.santos@telergystudio.com>
2026-07-13 20:11:09 -0300

    refactor: Organize operator classes and streamline registration process

--- __init__.py
+++ __init__.py
@@ -152,54 +152,6 @@ from .tgr_preferences import TGR_Preferences
 CLASSES_TO_REGISTER = (
     # Preferences
     TGR_Preferences,
-    # Operators
-    TGR_OT_AddDeformBone,
-    TGR_OT_AddNonDeformBone,
-    TGR_OT_AddPivotController,
-    TGR_OT_AddPrefix,
-    TGR_OT_AddSuffix,
-    TGR_OT_AddTGRArmature,
-    TGR_OT_AlignBoneToWorld,
-    TGR_OT_AutoColorBones,
-    TGR_OT_AutoCorrectUseDeform,
-    TGR_OT_BindORG,
-    TGR_OT_BindSwitch,
-    TGR_OT_BoneOnPoints,
-    TGR_OT_BonesOnVertices,
-    TGR_OT_CleanNameUp,
-    TGR_OT_CreateExecutable,
-    TGR_OT_ConnectBones,
-    TGR_OT_CopyTransforms,
-    TGR_OT_CopyTransformsToChain,
-    TGR_OT_CreateIKChain,
-    TGR_OT_CreateIKPoleTarget,
-    TGR_OT_CreateIntermediateBone,
-    TGR_OT_CreateSingleControllerStretch,
-    TGR_OT_CreateRotationChain,
-    TGR_OT_CreateSwitchChains,
-    TGR_OT_CreateTweakChain,
-    TGR_OT_FKFromTweakChain,
-    TGR_OT_CreateORG,
-    TGR_OT_RenameCollection,
-    TGR_OT_UIPicker,
-    TGR_OT_GenerateUI,
-    TGR_OT_IsolateBone,
-    TGR_OT_LoadCollections,
-    TGR_OT_LockBonesFromCollection,
-    TGR_OT_ParentToRoot,
-    TGR_OT_RemoveCollection,
-    TGR_OT_RemovePrefix,
-    TGR_OT_RemoveSuffix,
-    TGR_OT_RemoveORG,
-    TGR_OT_SaveCollections,
-    TGR_OT_SelectBonesByName,
-    TGR_OT_SelectCollectionBones,
-    TGR_OT_SetCollectionActive,
-    TGR_OT_ToggleDeformerConstraint,
-    TGR_OT_AssignBonesToCollection,
-    TGR_OT_NewCollection,
-    TGR_OT_UnbindORG,
-    TGR_OT_SampleTransforms,
     # Panels
     TGR_PT_View3D_Panel_BoneCollections,
     TGR_PT_View3D_Panel_CustomProperties,
@@ -335,6 +287,7 @@ def header_draw_menu(self, context):
 def register():
     # Register classes
     properties.register()
+    operators.register()
 
     for cls in CLASSES_TO_REGISTER:
         bpy.utils.register_class(cls)
@@ -413,4 +366,5 @@ def unregister():
     for cls in CLASSES_TO_REGISTER:
         bpy.utils.unregister_class(cls)
     
+    operators.unregister()
     properties.unregister()
--- operators/__init__.py
+++ operators/__init__.py
@@ -29,8 +29,7 @@ from .tgr_pose_mode_operators import (TGR_OT_AddPivotController,
                                       TGR_OT_UnbindORG,
                                       TGR_OT_SampleTransforms,)
 # Add classes from tgr_ui_operators
-from .tgr_ui_operators import (TGR_OT_GenerateUI,
-                               TGR_OT_UIPicker,
+from .tgr_ui_operators import (
                                TGR_OT_CreateExecutable,
                                )
 # Add classes from tgr_utilities_operators
@@ -52,5 +51,77 @@ from .tgr_utilities_operators import (TGR_OT_AddPrefix,
                                       TGR_OT_SelectCollectionBones,
                                       TGR_OT_SetCollectionActive,)
 # Add classes from tgr_weight_paint_operators
-from .tgr_weight_paint_operators import (TGR_OT_ToggleDeformerConstraint,)
+from .tgr_weight_paint_operators import (TGR_OT_ToggleDeformerConstraint, TGR_OT_ActivateBrush)
+
+
+
+OPERATOR_CLASSES = [
+    # Edit Mode Operators
+    TGR_OT_AddDeformBone,
+    TGR_OT_AddNonDeformBone,
+    TGR_OT_AlignBoneToWorld,
+    TGR_OT_BoneOnPoints,
+    TGR_OT_BonesOnVertices,
+    TGR_OT_ConnectBones,
+    TGR_OT_CopyTransforms,
+    TGR_OT_CreateIntermediateBone,
+    TGR_OT_CreateORG,
+    TGR_OT_CreateSwitchChains,
+    TGR_OT_ParentToRoot,
+    TGR_OT_RemoveORG,
+
+    # Object Mode Operators
+    TGR_OT_AddTGRArmature,
+
+    # Pose Mode Operators
+    TGR_OT_AddPivotController,
+    TGR_OT_BindORG,
+    TGR_OT_BindSwitch,
+    TGR_OT_CopyTransformsToChain,
+    TGR_OT_CreateIKChain,
+    TGR_OT_CreateIKPoleTarget,
+    TGR_OT_CreateRotationChain,
+    TGR_OT_CreateSingleControllerStretch,
+    TGR_OT_CreateTweakChain,
+    TGR_OT_FKFromTweakChain,
+    TGR_OT_IsolateBone,
+    TGR_OT_UnbindORG,
+    TGR_OT_SampleTransforms,
+
+    # UI Operators
+    TGR_OT_CreateExecutable,
+
+    # Utilities Operators
+    TGR_OT_AddPrefix,
+    TGR_OT_AddSuffix,
+    TGR_OT_AssignBonesToCollection,
+    TGR_OT_AutoColorBones,
+    TGR_OT_AutoCorrectUseDeform,
+    TGR_OT_CleanNameUp,
+    TGR_OT_LoadCollections,
+    TGR_OT_LockBonesFromCollection,
+    TGR_OT_NewCollection,
+    TGR_OT_RemoveCollection,
+    TGR_OT_RemovePrefix,
+    TGR_OT_RemoveSuffix,
+    TGR_OT_RenameCollection,
+    TGR_OT_SaveCollections,
+    TGR_OT_SelectBonesByName,
+    TGR_OT_SelectCollectionBones,
+    TGR_OT_SetCollectionActive,
+
+    # Weight Paint Operators
+    TGR_OT_ToggleDeformerConstraint,
+    TGR_OT_ActivateBrush,
+]
+
+
+def register():
+    for cls in OPERATOR_CLASSES:
+        bpy.utils.register_class(cls)
+
+def unregister():
+    for cls in reversed(OPERATOR_CLASSES):
+        bpy.utils.unregister_class(cls)
+
 
